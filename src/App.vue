b952986748f8a0dcf4815d9e743a4e7aaaf906f4
jacekmatuszyk <matuszykjacek@gmail.com>
2021-11-03 11:16:04 +0100

    added functionality to prev and next buttons

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@
           <h2 class="song-title">{{ current.title }} <span>- {{ current.artist }}</span> </h2>
 
           <div class="controls">
-            <button class="prev">Prev</button>
+            <button class="prev" @click="prev">Prev</button>
             <button class="play" v-if="!isPlaying" @click="play">Play</button>
             <button class="pause" v-else @click="pause">Pause</button>
-            <button class="next">Next</button>
+            <button class="next" @click="next">Next</button>
 
           </div>
 
@@ -20,9 +20,7 @@
       <section class="playlist">
         <h3>The Playlist</h3>
 
-        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing': 'song'">
-          {{ song.title }}
-        </button>
+        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing': 'song'"> {{ song.title }} - {{ song.artist }} </button>
 
       </section>
     </main>
@@ -62,17 +60,32 @@
       play(song) {
         if (typeof song.src != "undefined") {
           this.current = song;
-
           this.player.src = this.current.src;
-        }
-        
+        } 
         this.player.play()
         this.isPlaying = true;
-
       },
       pause () {
         this.player.pause();
         this.isPlaying = false;
+      },
+      prev () {
+        this.index--;
+        if (this.index < 0) {
+          this.index = this.songs.length - 1;
+        }
+
+        this.current = this.songs[this.index];
+        this.play(this.current);
+      },
+      next () {
+        this.index++;
+        if (this.index > this.songs.length - 1) {
+          this.index = 0;
+        }
+
+        this.current = this.songs[this.index];
+        this.play(this.current);
       }
     },
     created () {
